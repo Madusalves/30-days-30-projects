c58b88588744d1482ffb9215c1ba7a436747e3dc
Maria Eduarda <malvesduarte444@gmail.com>
2023-08-09 22:08:51 -0300

    last commit

--- day 3/notes.js
+++ day 3/notes.js
@@ -2,6 +2,12 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelector(".input-box");
 
+/*function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();*/
+
 function updateStorage() {
     localStorage.setItem("notes", notesContainer.innerHTML);
 }
@@ -21,12 +27,19 @@ notesContainer.addEventListener("click", function(e){
         e.target.parentElement.remove();
         updateStorage();
     }
-    else if(e.target.tagName === "p"){
-        notes = document.querySelector(".input-box");
-        notes.forEach(element => {
-            notes.okeyup = function(){
+    else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
                 updateStorage();
             }
-        });
+        })
+    }
+})
+
+document.addEventListener("keydown", event =>{
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
 })
